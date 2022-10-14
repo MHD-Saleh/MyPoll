26a16b9b3428875e44e53bffab20ac942346bd80
MHD-Saleh <saleh.alsaleh.1998@hotmail.com>
2022-10-14 13:07:31 +0300

    Update dbconf.js

--- dbconf.js
+++ dbconf.js
@@ -1,11 +1,19 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
+  host: "postgres://my_poll_user:bcntGadORJ1ACDwqibqKI3NpMbNKkfcv@dpg-cd4ibdqrrk02t5f1ibng-a.oregon-postgres.render.com/my_poll",
+  user: "my_poll_user",
+  password: "bcntGadORJ1ACDwqibqKI3NpMbNKkfcv",
+  database: "my_poll",
+  port: 5432,
+});
+
+/*const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
   password: "",
   database: "mypoll",
   port: 3308,
-});
+}); */
 
 connection.connect((error) => {
   if (error) {
